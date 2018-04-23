856b5385fecf7b86b136da3f09e704c0c20e0105
zhangmason <masonzhangmn@gmail.com>
2018-04-23 16:47:18 -0400

    Arduino code

--- Software/Arduino/main/defines.h
+++ Software/Arduino/main/defines.h
@@ -1,52 +1,73 @@
 //Servos
-
 #define X_STEP_PIN         54
 #define X_DIR_PIN          55
 #define X_ENABLE_PIN       38
-#define X_MIN_PIN           3
-#define X_MAX_PIN           2
 
 #define Y_STEP_PIN         60
 #define Y_DIR_PIN          61
 #define Y_ENABLE_PIN       56
-#define Y_MIN_PIN          14
-#define Y_MAX_PIN          15
 
 #define Z_STEP_PIN         46
 #define Z_DIR_PIN          48
 #define Z_ENABLE_PIN       62
-#define Z_MIN_PIN          18
-#define Z_MAX_PIN          19
+
+#define E_STEP_PIN         26
+#define E_DIR_PIN          28
+#define E_ENABLE_PIN       24
+
+#define Q_STEP_PIN         36
+#define Q_DIR_PIN          34
+#define Q_ENABLE_PIN       30
+
+#define SDPOWER            -1
+#define SDSS               53
+#define LED_PIN            13
+#define PS_ON_PIN          12
+#define KILL_PIN           -1
+
+
+
+enum PIECE_TYPE {
+    KING_T = 0,
+    QUEEN_T = 1,
+    ROOK_T = 2,
+    BISHOP_T = 3,
+    KNIGHT_T = 4,
+    PAWN_T = 5
+};
 
 enum PIECE_HEIGHTS {
-    KING = 41,
-    QUEEN = 34,
-    ROOK = 20,
-    BISHOP = 28,
-    KNIGHT = 24,
-    PAWN = 19
+    K_H = 500,
+    Q_H = 500,
+    R_H = 500,
+    B_H = 500,
+    N_H = 500,
+    P_H = 2800
+};
+
+const int sqy = 330;
+const int sqx = 260;
+
+enum Y_POSITIONS {
+    A = 260,
+    B = A + sqy,
+    C = B + sqy,
+    D = C + sqy,
+    E = D + sqy,
+    F = E + sqy,
+    G = F + sqy,
+    H = G + sqy,
 };
 
 enum X_POSITIONS {
-    A = 100,
-    B = 115,
-    C = 130,
-    D = 145,
-    E = 160,
-    F = 175,
-    G = 190,
-    H = 205,
-};
-
-enum Y_POSITIONS {
-    ONE = 100,
-    TWO = 115,
-    THREE = 130,
-    FOUR = 145,
-    FIVE = 160,
-    SIX = 175,
-    SEVEN = 190,
-    EIGHT = 205,
+    ONE = 260,
+    TWO = ONE     + sqx,
+    THREE = TWO   + sqx,
+    FOUR = THREE  + sqx,
+    FIVE = FOUR   + sqx,
+    SIX = FIVE    + sqx,
+    SEVEN = SIX   + sqx,
+    EIGHT = SEVEN + sqx,
 };
 
 int captured_x = 220;
--- Software/Arduino/main/servos.ino
+++ Software/Arduino/main/servos.ino
@@ -8,27 +8,33 @@ void move() {
   int captured_type = received_command[6];
 
   //if piece_captured == 1:
-    //move x and y by x_end and y_end
-    //move z by captured_type
-    activate_electromagnet(true);
-    //move z by -captured_type
-    //move x by captured_x
-    //move y by captured_y
-    captured_y += 15;
-    //move z by captured_type
-    activate_electromagnet(false);
-    //move z by -captured_type
-    //move x and y by -x_end and -y_end
-    
-  //move x and y by x_start and y_start
-  //move z by piece_type
-  activate_electromagnet(true);
-  //move z by -piece_type
-  //move x and y by (x_end-x_start) and (y_end-y_start)
-  //move z by piece_type
-  activate_electromagnet(false);
-  //move z by -piece_type
+  
+  //move x and y by x_end and y_end
+  //move z by captured_type
+  /////activate_electromagnet(true);
+  //move z by -captured_type
+  //move x by captured_x
+  //move y by captured_y
+  /////captured_y += 15;
+  //move z by captured_type
+  /////activate_electromagnet(false);
+  //move z by -captured_type
   //move x and y by -x_end and -y_end
+  
+  move_x(x_start); //move x and y by x_start and y_start
+  move_y(y_start);
+  delay(3000);
+  //move z by piece_type
+  //activate_electromagnet(true);
+  //move z by -piece_type
+  move_x(x_end - x_start); //move x and y by (x_end-x_start) and (y_end-y_start)
+  move_y(y_end - y_start);
+  delay(3000);
+  //move z by piece_type
+  //activate_electromagnet(false);
+  //move z by -piece_type
+  move_x(-x_end);//move x and y by -x_end and -y_end
+  move_x(-y_end);
 }
 
 void activate_electromagnet(boolean on) {
@@ -40,15 +46,74 @@ void activate_electromagnet(boolean on) {
 }
 
 void move_x(int amount) {
-  
+  digitalWrite(X_ENABLE_PIN    , LOW);
+  digitalWrite(Y_ENABLE_PIN    , LOW);
+
+  int start = millis();
+  while (millis() - start < abs(amount)) {
+    if (amount > 0) {
+      digitalWrite(X_DIR_PIN    , HIGH);
+      digitalWrite(Y_DIR_PIN    , HIGH);
+    } else {
+      digitalWrite(X_DIR_PIN    , LOW);
+      digitalWrite(Y_DIR_PIN    , LOW);
+    }
+    move_help();
+  }
+
+  digitalWrite(X_ENABLE_PIN    , HIGH);
+  digitalWrite(Y_ENABLE_PIN    , HIGH);
 }
 
 void move_y(int amount) {
-  
+  digitalWrite(E_ENABLE_PIN    , LOW);
+  digitalWrite(Q_ENABLE_PIN    , LOW);
+
+  int start = millis();
+  while (millis() - start < abs(amount)) {
+    if (amount > 0) {
+      digitalWrite(E_DIR_PIN    , HIGH);
+      digitalWrite(Q_DIR_PIN    , HIGH);
+    } else {
+      digitalWrite(E_DIR_PIN    , LOW);
+      digitalWrite(Q_DIR_PIN    , LOW);
+    }
+    move_help();
+  }
+
+  digitalWrite(E_ENABLE_PIN    , HIGH);
+  digitalWrite(Q_ENABLE_PIN    , HIGH);
 }
 
 void move_z(int amount) {
-  
+  delay(50);
+  digitalWrite(Z_ENABLE_PIN    , LOW);
+
+  int start = millis();
+  while (millis() - start < abs(amount)) {
+    if (amount > 0) {
+      digitalWrite(Z_DIR_PIN    , HIGH);
+    } else {
+      digitalWrite(Z_DIR_PIN    , LOW);
+          }
+    move_help();
+  }
+  digitalWrite(Z_ENABLE_PIN    , HIGH);
+  delay(50);
+}
+
+void move_help() {
+  digitalWrite(X_STEP_PIN    , HIGH);
+  digitalWrite(Y_STEP_PIN    , HIGH);
+  digitalWrite(Z_STEP_PIN    , HIGH);
+  digitalWrite(E_STEP_PIN    , HIGH);
+  digitalWrite(Q_STEP_PIN    , HIGH);
+  delay(.01);
+  digitalWrite(X_STEP_PIN    , LOW);
+  digitalWrite(Y_STEP_PIN    , LOW);
+  digitalWrite(Z_STEP_PIN    , LOW);
+  digitalWrite(E_STEP_PIN    , LOW);
+  digitalWrite(Q_STEP_PIN    , LOW);
 }
 
 
